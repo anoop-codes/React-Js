4d9785c9de9fa09bd2e4d9d94e93d24c6c4fb285
anoop-codes <kumar.anoop.9826@gmail.com>
2020-07-27 18:46:12 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePartTwo from './part-2/HomePartTwo';
+
 
 function App() {
   return (
     <div className="container">
-      <HomePartTwo />
+
     </div>
   );
 }
